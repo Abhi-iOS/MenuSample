c5a87a4997b383036e27dfcc6d26a509a9b26d67
Appinventiv <appinventiv@Appinventivs-Mac-mini.local>
2017-02-23 18:42:53 +0530

    Menu side bar created

---
+++ MenuSample/RowCell.swift
@@ -0,0 +1,24 @@
+//
+//  RowCell.swift
+//  MenuSample
+//
+//  Created by Appinventiv on 23/02/17.
+//  Copyright © 2017 Appinventiv. All rights reserved.
+//
+
+import UIKit
+
+class RowCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
